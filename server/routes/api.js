db965e5b70eb2c69b16cc16832df0a4a812b3a46
Rongle <ronglecat@163.com>
2019-04-28 17:48:30 +0800

    江南接口测试

--- server/routes/api.js
+++ server/routes/api.js
@@ -23,4 +23,13 @@ router.get('/getPrintData', async function(ctx, next) {
   }
 })
 
+router.post('/getPrintData', async function(ctx, next) {
+  console.log(ctx.params);
+  console.log(ctx.request.body);
+  console.log(ctx.querys);
+  ctx.body = {
+    data: "OK"
+  }
+})
+
 module.exports = router
